d57304d34e0f2dac43f84ca9e8a7e63df33841e6
Basheernavan <basheerdublin@gmail.com>
2020-07-02 18:26:40 +0100

    basheer created first project

---
+++ src/firstpackage/basicjavaday1.java
@@ -0,0 +1,10 @@
+package firstpackage;
+
+public class basicjavaday1 {
+	public static void main(String[] args) {
+		System.out.println("Basheer first Project");
+		System.out.println("Hello world");
+		
+	}
+
+}
