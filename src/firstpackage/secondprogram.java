5d349aa10ad966ae16709bd1f259abf97f539ba7
Basheernavan <basheerdublin@gmail.com>
2020-07-02 18:41:44 +0100

    created second program declared int, double and long.

---
+++ src/firstpackage/secondprogram.java
@@ -0,0 +1,20 @@
+package firstpackage;
+
+public class secondprogram {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 30, c = 40, d=50;
+		System.out.println(a);
+		System.out.println(b+c+d);
+		long f = 333333222;
+		System.out.println(f);
+		double g = 20000.15;
+		System.out.println(g);
+		double h = 3000;
+		System.out.println(h);
+		
+				
+				
+	}
+
+}
