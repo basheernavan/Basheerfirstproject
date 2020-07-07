ef90d70dee9f4b647dad22764e071ce08639c127
Basheernavan <basheerdublin@gmail.com>
2020-07-07 14:22:03 +0100

    1.Table of 5 And 2.print from 45 to 28

---
+++ src/pack3/PrintTableOf5.java
@@ -0,0 +1,12 @@
+package pack3;
+
+public class PrintTableOf5 {
+public static void main(String[] args) {
+	
+	for(int i=1;i<=10;i=i+1) {
+				
+		System.out.println(5+"*"+i+"="+(5*i));
+	}
+}
+}
+
---
+++ src/pack3/printhenosfrom45To28.java
@@ -0,0 +1,11 @@
+package pack3;
+
+public class printhenosfrom45To28 {
+public static void main(String[] args) {
+	int i=45;//initialization;
+	while(i>=28) {//condition
+		System.out.println("Printing i value: "+i);
+		i--;//decremental
+}
+}
+}
